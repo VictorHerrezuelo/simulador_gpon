3817a085a0f54004a89c089a63ce13b3b2ce336e
VictorHerrezuelo <victorherrezuelo@gmail.com>
2023-11-28 23:21:36 +0100

    Realizado simulaciones para carga=0.8, t_sim=10s

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 ### Importamos módulos propios
 from ejecutar_simulacion import ejecutar_simulacion
+from calcular_estadisticas import calcular_estadisticas
 
 
 ## Simulación
 def main():
+    # Ejecutamos la simulacion
     ejecutar_simulacion()
+    # Calculamos las estadísticas
+    calcular_estadisticas()
 
 if __name__ == '__main__':
     main()
--- packages/configuration/configuration.py
+++ packages/configuration/configuration.py
@@ -1,10 +1,10 @@
 # A través de este fichero configuramos rápidamente los parámetros relevantes de la simulación.
-CONFIG_T_SIM=.01
+CONFIG_T_SIM=10
 CONFIG_CARGA=.8
 
 ## Configuración
 multiples_colas=False
 insertionmethod_separatequeue0_priorityqueue1=True
 watch_on=False
-mostrar_progreso=False
-mostrar_profiling=False
+mostrar_progreso=True
+mostrar_profiling=True
